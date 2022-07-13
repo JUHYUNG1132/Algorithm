577cf75a886b3acae0110d0c31dcbbc3c09d9304
jeff0507 <jeff0507@naver.com>
2022-07-13 14:16:06 +0900

    FEAT: 평균 실행시간 추가

--- 개념/정렬/mklst.py
+++ 개념/정렬/mklst.py
@@ -1,2 +1,29 @@
 import random
-def mklst(len):
+import time
+
+def mklst(len, min=-10, max=10):
+    lst = list()
+    for i in range(len):
+        lst.append(random.randint(min,max))
+    return lst
+
+
+def avg_time(func, target, epoch = 10):
+    """
+    :param func: target function(without parenthesis)
+    :param target: target list
+    :param epoch: repeat time(default=10
+    :return: average run-time(int)
+    """
+    t = list()
+    for i in range(epoch):
+        lst = list(target)
+
+        s = time.time()
+        func(lst)
+        t.append(time.time()-s)
+
+    return sum(t)/len(t)
+
+if __name__ == "__main__":
+    print(mklst(20))
